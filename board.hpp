0bfaea97bd217beb99da7a23ba79691d67a42b12
Poly1581 <bpaul006@ucr.edu>
2023-06-30 14:55:31 -0700

    Added rough board representation - next step track for en passant and castling

--- board.hpp
+++ board.hpp
@@ -1,10 +1,22 @@
 #ifndef BOARD_HPP
 #define BOARD_HPP
 
+#include <string>
+using std::string;
+
 class board {
 	private:
 		//Starting with using char array to keep track of board state
-		
+		char state[8][8] = {
+			'r', 'n', 'b', 'k', 'q', 'b', 'n', 'r',
+			'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p',
+			'_', '_', '_', '_', '_', '_', '_', '_',
+			'_', '_', '_', '_', '_', '_', '_', '_',
+			'_', '_', '_', '_', '_', '_', '_', '_',
+			'_', '_', '_', '_', '_', '_', '_', '_',
+			'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P',
+			'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'
+		};
 	public:
 };
 
